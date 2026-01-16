c5cab367cf8bd5329eb945bc055e1f9432078e5a
lexsteine <lexsteine@gmail.com>
2026-01-16 21:34:33 +0300

    Мой первый коммит в отдельную ветку. К 'базе' проекта добавлены только id-шники ботов

--- db/OrderStatuses.py
+++ db/OrderStatuses.py
@@ -4,19 +4,13 @@ table_name = 'Clients'
 
 async def create_clients():
     async with aiosqlite.connect(DB_NAME) as db:
-#         await db.execute(f'''
-# CREATE TABLE IF NOT EXISTS {DB_NAME} (
-# user_id INTEGER PRIMARY KEY, 
-# question_index INTEGER, 
-# records_book VARCHAR(255))''')
 
-# Добавил свою модельку клиентов
+# Добавил свою модельку статусов заказов
 # Может не заработать, т.к. скрипт написан под YQL
         await db.execute(f'''
-CREATE TABLE IF NOT EXISTS Clients (
+CREATE TABLE IF NOT EXISTS OrderStatuses (
 id uint64 NOT NULL,
+code Utf8 NOT NULL,
 name Utf8 NOT NULL,
-phone Utf8 NOT NULL,
-username Utf8 NOT NULL,
 PRIMARY KEY (id));''')
         await db.commit()
--- db/WorkTypes.py
+++ db/WorkTypes.py
@@ -4,19 +4,14 @@ table_name = 'Clients'
 
 async def create_clients():
     async with aiosqlite.connect(DB_NAME) as db:
-#         await db.execute(f'''
-# CREATE TABLE IF NOT EXISTS {DB_NAME} (
-# user_id INTEGER PRIMARY KEY, 
-# question_index INTEGER, 
-# records_book VARCHAR(255))''')
 
-# Добавил свою модельку клиентов
+# Добавил свою модельку видов работ
 # Может не заработать, т.к. скрипт написан под YQL
         await db.execute(f'''
-CREATE TABLE IF NOT EXISTS Clients (
+CREATE TABLE IF NOT EXISTS WorkTypes (
 id uint64 NOT NULL,
+code Utf8 NOT NULL,
 name Utf8 NOT NULL,
-phone Utf8 NOT NULL,
-username Utf8 NOT NULL,
+duration uint64 NULL,
 PRIMARY KEY (id));''')
         await db.commit()
